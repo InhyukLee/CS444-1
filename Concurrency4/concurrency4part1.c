11c1018c9cbbd0db3668ad5e5f8e8de8a14cc8e2
alessanf <alessandro.lin@hotmail.com>
2017-05-31 21:06:51 -0700

    part1 is bugged, fixing it

--- Concurrency4/concurrency4part1.c
+++ Concurrency4/concurrency4part1.c
@@ -12,27 +12,65 @@
 #include <unistd.h>
 #include <time.h>
 #include <semaphore.h>
-sem_t available_resource;
-sem_t check_resource;
+sem_t available_r;
+sem_t check_r;
 
-void *do_something(int id)
+void use_resource(void *id)
 {
+	int *u_id = (int *)id;
+	printf("thread %d: using resource.\n", *u_id);
+	fflush(stdout);
+	sleep(rand()%3+1);
+	printf("thread %d: is finished.\n", *u_id);
+	fflush(stdout);
+}
+
+void *use(void *id)
+{
+	int *u_id = (int *)id;
+	int free_r;
 	while(1) {
+		sem_getvalue(&available_r, &free_r);
+		if(free_r == 0) {
+			printf("thread %d: waiting for resource to be free.\n", *u_id);
+			fflush(stdout);
+			sem_wait(&check_r);
+			sem_post(&check_r);
+		}
+		else if(free_r == 1) {
+			sem_wait(&check_r);
+		}
+		sem_wait(&available_r);
+		
 		use_resource(id);
+		
+		sem_post(&available_r);
+		sem_getvalue(&available_r, &free_r);
+		if(free_r == 3) {
+			sem_getvalue(&check_r, &free_r);
+			if(free_r == 0) {
+				sem_post(&check_r);
+			}
+		}
 	}
 }
 
-void use_resource(int id)
-{
-	printf("thread %s: using resource.", id);
-	int i = rand()%5+1;
-	sleep(i);
-}
-
 int main()
-{
-	sem_init(&available_resource, 0, 3);
-	sem_init(&check_resource, 0, 1);
+{	
+	pthread_t user[6];
+	int i;
+	int id[6];
+	for(i = 0; i<6;i++) {
+		id[i]= i;
+	}
+	sem_init(&available_r, 0, 3);
+	sem_init(&check_r, 0, 1);
 	srand(time(NULL));
+	for(i = 0; i < 6; i++) {
+		pthread_create(&user[i], NULL, use, &id[i]);
+	}
+	for(i = 0; i < 6; i++) {
+		pthread_join(user[i], NULL);
+	}
 	return 0;
 }
